3b11bb3b526e7addf084510601534ebbbf426ae6
xkored <xkored@gmail.com>
2019-03-31 16:47:25 +0400

    created sub serv item page

--- app/js/common.js
+++ app/js/common.js
@@ -1,90 +1,134 @@
 $('.hot__cnt').owlCarousel({
-      nav: true,
-      dots: false,
-      items: 3,
-      margin: 20,
-      navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
-      responsive : {
-        0   : {
-            items: 1
-        },
-        380 : {
-            items: 1
-        },
-        480 : {
-            items: 1
-        },
-        768 : {
-            items: 2
-        },
-        1040 : {
-            items: 3
-        }
+    nav: true,
+    dots: false,
+    items: 3,
+    margin: 20,
+    navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+    responsive : {
+      0   : {
+          items: 1
+      },
+      380 : {
+          items: 1
+      },
+      480 : {
+          items: 1
+      },
+      768 : {
+          items: 2
+      },
+      1040 : {
+          items: 3
       }
-  });
+    }
+});
 $('.cert__cnt').owlCarousel({
-      nav: true,
-      dots: false,
-      items: 6,
-      margin: 20,
-      navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
-      responsive : {
-        0   : {
-            items: 1
-        },
-        380 : {
-            items: 1
-        },
-        480 : {
-            items: 2
-        },
-        768 : {
-            items: 3
-        },
-        1040 : {
-            items: 4
-        },
-        1200 : {
-            items: 6
-        }
+    nav: true,
+    dots: false,
+    items: 6,
+    margin: 20,
+    navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+    responsive : {
+      0   : {
+          items: 1
+      },
+      380 : {
+          items: 1
+      },
+      480 : {
+          items: 2
+      },
+      768 : {
+          items: 3
+      },
+      1040 : {
+          items: 4
+      },
+      1200 : {
+          items: 6
       }
-  });
+    }
+});
 $('.videos__cnt').owlCarousel({
-      nav: true,
-      dots: false,
-      margin: 20,
-      navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
-      responsive : {
-        0   : {
-            items: 1
-        },
-        380 : {
-            items: 1
-        },
-        480 : {
-            items: 1
-        },
-        768 : {
-            items: 2
-        },
-        1040 : {
-            items: 3
-        }
+    nav: true,
+    dots: false,
+    margin: 20,
+    navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+    responsive : {
+      0   : {
+          items: 1
+      },
+      380 : {
+          items: 1
+      },
+      480 : {
+          items: 1
+      },
+      768 : {
+          items: 2
+      },
+      1040 : {
+          items: 3
       }
+    }
+});
+$('.team__cnt').owlCarousel({
+    nav: true,
+    dots: false,
+    margin: 20,
+    navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+    responsive : {
+      0   : {
+          items: 1
+      },
+      380 : {
+          items: 1
+      },
+      480 : {
+          items: 1
+      },
+      768 : {
+          items: 2
+      },
+      1040 : {
+          items: 3
+      }
+    }
+});
+$('.contacts__way-slider').owlCarousel({
+    nav: true,
+    items: 1,
+    loop: true,
+    dots: false,
+    navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+});
+$('.service_sub__slider').owlCarousel({
+    nav: true,
+    items: 1,
+    loop: true,
+    dots: false,
+    navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+});
+$('.facility__slider').owlCarousel({
+    nav: true,
+    items: 1,
+    loop: true,
+    dots: false,
+    navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"]
+});
+// tabs 
+$(document).ready(function () {
+  $(".tabs__content-item").hide();
+  $(".tabs__container div.tabs__content-item.active-tab").show();
+  $('ul.tabs__list > li').click(function () {
+    if (!($(this).hasClass('active'))) {
+      var thisLi = $(this);
+      var numLi = thisLi.index();
+      thisLi.addClass('active').siblings().removeClass('active');
+      thisLi.parent().next().children('div').hide().eq(numLi).fadeIn('slow');
+    }
   });
-	// tabs 
-	// $(".tabs__container div.tab-content__item:first-child").addClass('active');
-	// $(".main-slider .main-slider__bg-item:first-child").addClass('active');
-	//     $('ul.tabs__list > li').click(function () {
-	//         if (!($(this).hasClass('active'))) {
-	//             var thisLi = $(this);
-	//             var numLi = thisLi.index(); 
-	//             thisLi.addClass('active').siblings().removeClass('active');
-	//             thisLi.parent().next().children('div').removeClass('active').eq(numLi).addClass('active','slow');
-	//             thisLi.parent().parent().parent().parent().find('.main-slider__bg').children('.main-slider__bg-item').removeClass('active','slow').eq(numLi).addClass('active','slow');
-	//             $('.reviews__cnt_text').owlCarousel('refresh');
-	//         }
- //    });
+});
  if ( $(window).width() > 1000 ) {
   $('.header__search-submit').click(function() {
       $('.header__search').toggleClass('active');
